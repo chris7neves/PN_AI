ff8f84e88232916a67d04573e7425c7393a5e269
Riccardo Restagno <riccardo.restagno@gmail.com>
2019-11-24 18:27:49 -0500

    Heat Map first iteration

---
+++ HeatMapGenerator.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import openpyxl
+
+
+def generate_heat_map(excelFile, sheetNumber, trailNumber):
+    row_start = 18
+    column_start = 16 + (trailNumber - 1) * 3
+
+    wb = openpyxl.load_workbook(f'Excel_Files\\{excelFile}.xlsm')
+    worksheet = wb[f'Sheet{sheetNumber}']
+    x, y = get_coordinate_arrays(worksheet, row_start, column_start)
+
+    heatmap, xedges, yedges = np.histogram2d(x, y, bins=30)
+    extent = [0, 1, 0, 1]
+
+    plt.clf()
+    plt.imshow(heatmap.T, extent=extent, origin='lower')
+    plt.show()
+
+
+def get_coordinate_arrays(worksheet, rowStart, columnStart):
+    x_coordinate_array = []
+    y_coordinate_array = []
+
+    for row_cells in worksheet.iter_rows(min_row=rowStart, min_col=columnStart, max_col=columnStart + 1):
+        for index, cell in enumerate(row_cells):
+            try:
+                float(cell.value)
+            except ValueError:
+                break
+            if index == 0:
+                x_coord = float(cell.value)
+                continue
+            else:
+                y_coord = float(cell.value)
+
+            if x_coord <= 0 or y_coord <= 0:
+                break
+            x_coordinate_array.append(x_coord)
+            y_coordinate_array.append(y_coord)
+
+    return np.array(x_coordinate_array), np.array(y_coordinate_array)
+
+
+if __name__ == "__main__":
+    print("-----------HEAT MAP GENERATOR-----------")
+    excelFile = input("Enter the Excel file name you would like to generate a heatmap for:\n")
+    sheetNumber = int(input("Enter the sheet number you would like to generate a heatmap for (1-11):\n"))
+    trailNumber = int(input("Enter the trial number you would like to generate a heatmap for (1-8):\n"))
+    generate_heat_map(excelFile, sheetNumber, trailNumber)
