fdf05d368a9f2a70e10c0c7ab1000dccd6e802d0
Riccardo Restagno <riccardo.restagno@gmail.com>
2020-02-06 22:11:17 -0500

    Added support for heatmap output (needs work)

--- HeatMapGenerator.py
+++ HeatMapGenerator.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.ndimage.filters import gaussian_filter
+import matplotlib.cm as cm
 import openpyxl
 
 
@@ -10,22 +11,34 @@ TRIAL_TYPES = ["Practice Front-Front", "Front-Front Condition 1M", "Front-Front 
                "Inverted 1F"]
 
 
-def generate_heat_map(excelFile, sheetNumber):
-
-    wb = openpyxl.load_workbook(f'Data\\{excelFile}.xlsm')
-    worksheet = wb[f'Sheet{sheetNumber}']
+def generate_scatter_plot(excel_file, sheet_number):
+    wb = openpyxl.load_workbook(f'Data\\{excel_file}.xlsm')
+    worksheet = wb[f'Sheet{sheet_number}']
     fig, axs = plt.subplots(3, 3)
-    trialNumber = 1
+    trial_number = 1
 
-    for ax, in zip(axs.flatten()):
+    for ax in zip(axs.flatten()):
         row_start = 18
-        column_start = 16 + (trialNumber - 1) * 3
+        column_start = 16 + (trial_number - 1) * 3
         x, y = get_coordinate_arrays(worksheet, row_start, column_start)
         ax.plot(x, y, 'k.', markersize=5)
-        trialNumber += 1
-        if trialNumber == 9:
+        trial_number += 1
+        if trial_number == 9:
             break
-    fig.suptitle(f"Test subject {excelFile} - {TRIAL_TYPES[sheetNumber-1]}")
+
+    fig.suptitle(f"Test subject {excel_file} - {TRIAL_TYPES[sheet_number - 1]}")
+    plt.show()
+
+
+def generate_heat_map(excel_file, sheet_number, trial_number):
+    row_start = 18
+    column_start = 16 + (trial_number - 1) * 3
+    wb = openpyxl.load_workbook(f'Data\\{excel_file}.xlsm')
+    worksheet = wb[f'Sheet{sheet_number}']
+    x, y = get_coordinate_arrays(worksheet, row_start, column_start)
+
+    img, extent = myplot(x, y, 32)
+    plt.imshow(img, extent=extent, origin='lower', cmap=cm.jet)
     plt.show()
 
 
@@ -33,7 +46,8 @@ def myplot(x, y, s, bins=500):
     heatmap, xedges, yedges = np.histogram2d(x, y, bins=bins)
     heatmap = gaussian_filter(heatmap, sigma=s)
 
-    extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+    # extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+    extent = [-0.2, 1.2, -0.2, 1.2]
     return heatmap.T, extent
 
 
@@ -62,7 +76,19 @@ def get_coordinate_arrays(worksheet, rowStart, columnStart):
 
 
 if __name__ == "__main__":
-    print("-----------HEAT MAP GENERATOR POC-----------")
-    excelFile = input("Enter the Excel file name from the Data folder you would like to generate scatter plots for:\n")
-    sheetNumber = int(input("Enter the sheet number you would like to generate scatter plots for (1-11):\n"))
-    generate_heat_map(excelFile, sheetNumber)
+    scatter_plot = False
+    heat_map = True
+    if scatter_plot:
+        print("-----------SCATTER PLOT GENERATOR POC-----------")
+        excel_file = input(
+            "Enter the Excel file name from the Data folder you would like to generate scatter plots for:\n")
+        sheet_number = int(input("Enter the sheet number you would like to generate scatter plots for (1-11):\n"))
+        generate_scatter_plot(excel_file, sheet_number)
+    elif heat_map:
+        print("-----------HEAT MAP GENERATOR POC-----------")
+        excel_file = input(
+            "Enter the Excel file name from the Data folder you would like to generate a heat map for:\n")
+        sheet_number = int(input("Enter the sheet number you would like to generate a heat map for (1-11):\n"))
+        trial_number = int(input("Enter the trial number you would like to generate a heat map for (1-8):\n"))
+        generate_heat_map(excel_file, sheet_number, trial_number)
+
