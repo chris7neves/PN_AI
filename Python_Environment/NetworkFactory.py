481521cee7d7505572b503660a03a295050ca853
chris7neves <christopher.neves@hotmail.ca>
2020-02-22 11:34:56 -0500

    small changes

--- Python_Environment/NetworkFactory.py
+++ Python_Environment/NetworkFactory.py
@@ -1,5 +1,8 @@
 #---------------------------------------------------------------------
-# This file will accept a list with all the hyperparameters entered
-# by the user in main. It will then use this list of hyperparameters
-# to define as many networks as necessary
+# This file will accept a list with all the hyperparameters entered #
+# by the user in main. It will then use this list of hyperparameters#
+# to define as many networks as necessary, train them, then return  #
+# either the individual network objects or just the accuracy and    #
+# performance of each network.                                      #
 #---------------------------------------------------------------------
+
