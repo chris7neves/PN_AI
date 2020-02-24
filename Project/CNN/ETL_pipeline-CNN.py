d0411bce4ee14ea324dd4cb186f60f4862ee3442
chris7neves <christopher.neves@hotmail.ca>
2020-02-23 23:09:22 -0500

    pipfile changes

--- Project/CNN/ETL_pipeline-CNN.py
+++ Project/CNN/ETL_pipeline-CNN.py
@@ -5,19 +5,11 @@
 # SQL fetches
 #---------------------------------------------------------------------
 
-# Imports
-
+# ---------- Imports ---------- #
 import torch
-import numpy as np
-import matplotlib
-import torch.utils.data
-import torch.nn as nn
-import torch.nn.functional as F
-import torchvision
-import torchvision.transforms as transforms
-import matplotlib.pyplot as plt
-import torch.optim as optim # library that contains optimization functions
-import pandas as pd
+import numpy
+# ----------------------------- #
 
 width = 1
 height = 1
+
