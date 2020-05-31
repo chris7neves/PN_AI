6da13338dd466774cba9f0db8782853683d95932
Massimo <Pietracoops@gmail.com>
2020-05-31 13:25:09 -0400

    data query for correct / incorrect responses

--- Project/SQL/Execute.py
+++ Project/SQL/Execute.py
@@ -36,3 +36,30 @@ print(Trials_From_One_Excel_File)  # [ T_AUTO_KEY, TRIAL_NAME, RESULT_TRIAL_1, .
 
 Eye_Tracker_Data_For_One_Sheet = P1.EyeTrackerData(Trials_From_One_Excel_File[0][0])  # Call EyeTrackerData with T_AUTO
 print(Eye_Tracker_Data_For_One_Sheet[0])  # Eye Tracker [D_auto, Trial Number, Row Number, X, Y, Time, H_auto, T_Auto ]
+
+
+print("================================Example 4==============================================")
+All_Excel_Files = P1.Participants()  # We call Participants which returns array as follows
+print(All_Excel_Files)             # [ H_AUTO_KEY, NAME_OF_EXCEL_FILE, AGE, SEX, ASD/TYPICAL]
+
+response = P1.Query("""select
+case when T1_C = T1_I then 1 else 0 end as T1_A,
+case when T2_C = T2_I then 1 else 0 end as T2_A,
+case when T3_C = T3_I then 1 else 0 end as T3_A,
+case when T4_C = T4_I then 1 else 0 end as T4_A,
+case when T5_C = T5_I then 1 else 0 end as T5_A,
+case when T6_C = T6_I then 1 else 0 end as T6_A,
+case when T7_C = T7_I then 1 else 0 end as T7_A,
+case when T8_C = T8_I then 1 else 0 end as T8_A,
+T1_S,
+T2_S,
+T3_S,
+T4_S,
+T5_S,
+T6_S,
+T7_S,
+T8_S
+from DATA_TRIALS
+where h_auto_key = 1000081""")
+
+print(response)
